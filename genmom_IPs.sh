c09c5a40b43c180b43a05ad85ad2a135bc40e38b
Alexandre C. Leite <62979484+alexandrecleite@users.noreply.github.com>
2026-04-09 14:14:06 -0300

    Add files via upload

--- genmom_IPs.sh
+++ genmom_IPs.sh
@@ -4,6 +4,7 @@
 # Use 600.00 seg de update 
 
 #Pegando IPs
+readonly IFACE=$(ip -4 a | grep -E "^[0-9]" | grep -e "LOWER_UP" | grep -v "lo" | cut -d" " -f2 | sed 's/://')
 readonly INT=$(hostname -I | cut -d" " -f1)
 readonly INT6=$(ip a | grep "inet6 fe80" | cut -d" " -f6)
 readonly EXT=$(curl -s -4 ifconfig.me)
@@ -17,6 +18,8 @@ INFO+="</txt><txtclick>gufw</txtclick>"
 
 # Tooltip
 MORE_INFO="<tool>"
+MORE_INFO+="Interface: ${IFACE}\n\n"
+MORE_INFO+="IP Local: ${INT}\n"
 MORE_INFO+="IPv6 Local: ${INT6}\n\n"
 MORE_INFO+="IP Externo: ${EXT}\n"
 MORE_INFO+="IPv6 Externo: ${EXT6}"
