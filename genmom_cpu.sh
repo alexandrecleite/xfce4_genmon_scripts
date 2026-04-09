5a02916eaf62014e3c55a94cb6e57fe358b66bef
Alexandre C. Leite <62979484+alexandrecleite@users.noreply.github.com>
2026-04-09 03:10:29 -0300

    Add files via upload

--- genmom_cpu.sh
+++ genmom_cpu.sh
@@ -3,8 +3,11 @@
 # Depende do Top, Grep e Awk
 # Use 3.00 seg de update 
 
-readonly LSCPU=$(lscpu | grep -e "modelo" -e "CPU MHz")
+readonly LSCPU=$(lscpu | grep -e "modelo" | column -t)
+readonly LSCPU2=$(lscpu | grep -e "CPU MHz" -e "CPU(s):" | column -t -R 2-6)
 readonly USO=$(top -bn1 | grep "CPU(s)" | awk '{print 100 - $8"%"}')
+readonly USO2=$(top -bn1 | grep "CPU(s)")
+
 
 # Panel
 INFO+="<txt>"
@@ -14,8 +17,10 @@ INFO+="</txt><txtclick>xfce4-taskmanager</txtclick>"
 
 # Tooltip
 MORE_INFO="<tool>"
-MORE_INFO+="CPUs:\n"
-MORE_INFO+="${LSCPU}"
+MORE_INFO+="CPUs:\n\n"
+MORE_INFO+="${LSCPU}\n"
+MORE_INFO+="${LSCPU2}\n\n"
+MORE_INFO+="${USO2}"
 MORE_INFO+="</tool>"
 
 
