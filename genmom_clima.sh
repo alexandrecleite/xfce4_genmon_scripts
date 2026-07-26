3ef43656a548b6ab4609b0c4522f25f8d6a7c146
Alexandre C. Leite <62979484+alexandrecleite@users.noreply.github.com>
2026-07-25 21:33:40 -0300

    Add files via upload
    Update para o clima abrir o Firefox na página do clima

--- genmom_clima.sh
+++ genmom_clima.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # by Alexandre Cerqueira Leite
 # Depende do pacote Curl
-# Use 1800.00 seg de update 
+# Use 1800.00 seg (meia hora) de update 
 
 readonly CLIMA=$(curl -s wttr.in/?format="+%t+%c")
-#readonly CLIMATIP=$(curl -s -H "Accept-Language: pt-br" wttr.in?format="+%l\n\n+%C+%c\n+Temp:%20%t\n+S.Termica:%20%f\n+Vento:%20%w\n+Chuva:%20%p\n+Umidade:%20%h\n+Indice%20UV:%20%u\n+Pressao:%20%P\\n")
-readonly CLIMATIP=$(curl -s wttr.in?format="+%l\n\n+%C+%c\n+Temp:%20%t\n+S.Termica:%20%f\n+Vento:%20%w\n+Chuva:%20%p\n+Umidade:%20%h\n+Indice%20UV:%20%u\n+Pressao:%20%P\\n")
+readonly CLIMATIP=$(curl -s -H "Accept-Language: pt-br" wttr.in?format="+%l\n\n+%C+%c\n+Temp:%20%t\n+S.Termica:%20%f\n+Vento:%20%w\n+Chuva:%20%p\n+Umidade:%20%h\n+Indice%20UV:%20%u\n+Pressao:%20%P\\n")
+#readonly CLIMATIP=$(curl -s wttr.in?format="+%l\n\n+%C+%c\n+Temp:%20%t\n+S.Termica:%20%f\n+Vento:%20%w\n+Chuva:%20%p\n+Umidade:%20%h\n+Indice%20UV:%20%u\n+Pressao:%20%P\\n")
 readonly UPD=$(date | cut -d" " -f5 | cut -d":" -f1,2)
 
 # Panel
 INFO+="<txt>"
 INFO+="<span weight='Bold' fgcolor='#FFFFFF'>Clima:</span>"
 INFO+="<span fgcolor='#FFFFFF'>${CLIMA}</span>"
-INFO+="</txt>"
+INFO+="</txt><txtclick>firefox "https://www.google.com/search?q=clima"</txtclick>"
 
 # Tooltip
 MORE_INFO="<tool>"
